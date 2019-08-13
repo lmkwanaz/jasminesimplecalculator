793bc91e6604db7b3dc669e247413cfcdbf56e3c
lmkwanaz <lmkwanaz.lwandile.mk38@gmail.com>
2019-08-13 11:45:01 +0200

    final work

--- jasmine-standalone-3.4.0/spec/addspec.js
+++ jasmine-standalone-3.4.0/spec/addspec.js
@@ -1,1 +1,10 @@
-describe("add numbers", funcdtion);
+describe("add numbers", function(){
+    it("suppose to add",function(){
+        expect(add(1, 2, 3, 4)).toBe(10);
+    });
+});
+describe("multiply numbers", function(){
+    it("suppose to multiply", function(){
+        expect(multiply(1, 2, 3, 4, 5)).toBe(120);
+    });
+});
