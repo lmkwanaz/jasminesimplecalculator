1fd083ed2271a68b8780014ef5e15891d7b4eb38
lmkwanaz <lmkwanaz.lwandile.mk38@gmail.com>
2019-09-09 13:17:53 +0200

    modified

--- jasmine-standalone-3.4.0/spec/addspec.js
+++ jasmine-standalone-3.4.0/spec/addspec.js
@@ -1,10 +1,9 @@
 describe("add numbers", function(){
     it("suppose to add",function(){
         expect(add(1, 2, 3, 4)).toBe(10);
-    });
-});
-describe("multiply numbers", function(){
-    it("suppose to multiply", function(){
-        expect(multiply(1, 2, 3, 4, 5)).toBe(120);
+        expect(add(20, 20)).toBe(40);
+        expect(add(15, 5)).toBe(20);
+        expect(add(50, 10)).toBe(60);
+        expect(add(70, 30)).toBe(100);
     });
 });
---
+++ jasmine-standalone-3.4.0/spec/multiplyspec.js
@@ -0,0 +1,9 @@
+describe("multiplication", function(){
+    it("suppose to multiply numbers",function(){
+        expect(multiply(1, 1)).toBe(1);
+        expect(multiply(10, 10)).toBe(100);
+        expect(multiply(2, 5)).toBe(10);
+        expect(multiply(3, 3)).toBe(9);
+        expect(multiply(6, 5)).toBe(30);
+    });
+});
